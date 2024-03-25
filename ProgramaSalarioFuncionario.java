953ec33c2a8341282e09ae8b3d3d39db293f2de8
Ricardodallarosiz <dallarosizr@gmail.com>
2024-03-25 15:12:48 -0300

    ex18

---
+++ ProgramaSalarioFuncionario.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProgramaSalarioFuncionario {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do funcionário: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a idade do funcionário: ");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite o sexo do funcionário (M/F): ");
+        char sexo = scanner.next().charAt(0);
+
+        System.out.print("Digite o salário fixo do funcionário: ");
+        double salarioFixo = scanner.nextDouble();
+
+        Funcionario funcionario = new Funcionario(nome, idade, sexo, salarioFixo);
+        funcionario.mostrarDados();
+
+        scanner.close(); 
+    }
+    
+}
