4429dedad675448fd602464743f190d39faac84e
Ricardodallarosiz <dallarosizr@gmail.com>
2024-03-25 15:04:48 -0300

    ex20

--- Professor.java
+++ Professor.java
@@ -6,7 +6,7 @@ public class Professor {
     public Professor(String nome, int nivel) {
         this.nome = nome;
         this.nivel = nivel;
-        setValorHoraAula(); 
+        setValorHoraAula(); // Configura o valor da hora/aula com base no nível do professor
     }
 
     private void setValorHoraAula() {
@@ -22,8 +22,19 @@ public class Professor {
                 break;
             default:
                 System.out.println("Nível de professor inválido. Definindo valor padrão para hora/aula.");
-                valorHoraAula = 12.00; 
+                valorHoraAula = 12.00; // Valor padrão para nível 1
         }
-    
-}
+    }
+
+    public double calcularSalario(int horasAula) {
+        return horasAula * valorHoraAula;
+    }
+
+    public void imprimirSalario(int horasAula) {
+        double salario = calcularSalario(horasAula);
+        System.out.println("Nome do Professor: " + nome);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Horas/Aula Trabalhadas: " + horasAula);
+        System.out.println("Salário: R$" + salario);
+    }
 }
